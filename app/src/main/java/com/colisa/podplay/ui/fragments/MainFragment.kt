962537344f04b0d143d9abd581bb2d01ab6dd707
mtali <emmanuelmtali@yahoo.com>
2021-08-10 14:16:14 -0700

    Add search action

--- app/src/main/java/com/colisa/podplay/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/colisa/podplay/ui/fragments/MainFragment.kt
@@ -1,14 +1,22 @@
 package com.colisa.podplay.ui.fragments
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import com.colisa.podplay.R
 import com.colisa.podplay.databinding.FragmentMainBinding
+import timber.log.Timber
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +25,30 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_main_menu, menu)
+        val searchMenuItem = menu.findItem(R.id.action_search)
+        val searchView = searchMenuItem.actionView as SearchView
+        searchView.queryHint = getString(R.string.search)
+        activity?.let {
+            val searchManager = it.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+            searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(it.componentName)
+            )
+        }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_search -> {
+                Timber.d("Search pressed!")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
