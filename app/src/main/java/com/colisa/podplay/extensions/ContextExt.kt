8ebf25c91fadfa61d0b4f4ba640aa3e752f4a565
mtali <emmanuelmtali@yahoo.com>
2021-09-03 21:31:54 +0300

    :) add extension to get AudioManager from any context instance!

--- app/src/main/java/com/colisa/podplay/extensions/ContextExt.kt
+++ app/src/main/java/com/colisa/podplay/extensions/ContextExt.kt
@@ -2,8 +2,14 @@ package com.colisa.podplay.extensions
 
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Context.AUDIO_SERVICE
 import android.content.Context.NOTIFICATION_SERVICE
+import android.media.AudioManager
 
 fun Context.notificationManager(): NotificationManager {
     return getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+}
+
+fun Context.getAudioManager(): AudioManager {
+    return getSystemService(AUDIO_SERVICE) as AudioManager
 }
