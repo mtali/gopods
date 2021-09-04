2c1d3c45a54cf752aab66e9ad6f2061f49e431dc
mtali <emmanuelmtali@yahoo.com>
2021-09-04 16:53:22 +0300

    Show media notification for current playing episode

--- app/src/main/java/com/colisa/podplay/player/GoPlayerService.kt
+++ app/src/main/java/com/colisa/podplay/player/GoPlayerService.kt
@@ -1,20 +1,28 @@
 package com.colisa.podplay.player
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.ResultReceiver
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
+import androidx.core.content.ContextCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
 import timber.log.Timber
 
 class GoPlayerService : MediaBrowserServiceCompat() {
 
     private lateinit var mediaSession: MediaSessionCompat
+
+    private lateinit var notificationManager: NotificationManager
 
     private val goAudioAttributes = AudioAttributes.Builder()
         .setContentType(C.CONTENT_TYPE_MUSIC)
@@ -36,9 +44,25 @@ class GoPlayerService : MediaBrowserServiceCompat() {
     // Media connection
     private lateinit var mediaSessionConnector: MediaSessionConnector
 
+    private var isForeground = false
+
     override fun onCreate() {
         super.onCreate()
         createMediaSession()
+
+        // Create notification manager
+        notificationManager =
+            NotificationManager(this, mediaSession.sessionToken, PlayerNotificationListener())
+
+//        notificationManager.showNotificationForPlayer(exoPlayer)
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        exoPlayer.apply {
+            stop()
+            clearMediaItems()
+        }
     }
 
     override fun onDestroy() {
@@ -53,9 +77,16 @@ class GoPlayerService : MediaBrowserServiceCompat() {
     }
 
     private fun createMediaSession() {
+        // Build a PendingIntent that can be used to launch the UI
+        val sessionPendingIntent =
+            packageManager?.getLaunchIntentForPackage(packageName)?.let { intent ->
+                PendingIntent.getActivity(this, 0, intent, 0)
+            }
+
         // Create a new media session
         mediaSession = MediaSessionCompat(this, "GoPodsMediaSession")
             .apply {
+                setSessionActivity(sessionPendingIntent)
                 isActive = true
             }
 
@@ -89,22 +120,33 @@ class GoPlayerService : MediaBrowserServiceCompat() {
      * Listen events from exoplayer
      */
     private inner class PlayerEventListener : Player.Listener {
-        override fun onPlaybackStateChanged(state: Int) {
+
+        override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
+            onPlayerStateOrPlayWhenReady(playWhenReady, exoPlayer.playbackState)
+        }
+
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            onPlayerStateOrPlayWhenReady(exoPlayer.playWhenReady, playbackState)
+        }
+
+        private fun onPlayerStateOrPlayWhenReady(playWhenReady: Boolean, state: Int) {
             when (state) {
-                Player.STATE_IDLE -> {
-                    Timber.d("Player idle")
-                }
+                Player.STATE_READY,
                 Player.STATE_BUFFERING -> {
-                    Timber.d("Player buffering")
+                    notificationManager.showNotificationForPlayer(exoPlayer)
+                    if (state == Player.STATE_READY) {
+                        if (!playWhenReady) {
+                            stopForeground(false)
+                            isForeground = false
+                        }
+                    }
                 }
-                Player.STATE_ENDED -> {
-                    Timber.d("Player ended")
-                }
-                Player.STATE_READY -> {
-                    Timber.d("Player ready")
+                else -> {
+                    notificationManager.hideNotification()
                 }
             }
         }
+
 
         override fun onPlayerError(error: PlaybackException) {
             Timber.d("onPlayerError: ${error.message}")
@@ -139,12 +181,54 @@ class GoPlayerService : MediaBrowserServiceCompat() {
             Unit
 
         override fun onPrepareFromUri(uri: Uri, whenReady: Boolean, extras: Bundle?) {
-            val item = MediaItem.fromUri(uri)
+            val item = extractMediaItem(uri, extras)
             exoPlayer.apply {
                 setMediaItem(item)
                 prepare()
                 playWhenReady = whenReady
             }
+
+        }
+
+        private fun extractMediaItem(uri: Uri, extras: Bundle?): MediaItem {
+            return if (extras == null) {
+                MediaItem.fromUri(uri)
+            } else {
+                MediaItem.Builder()
+                    .setUri(uri)
+                    .setMediaMetadata(
+                        MediaMetadata.Builder()
+                            .setTitle(extras.getString(MediaMetadataCompat.METADATA_KEY_TITLE))
+                            .setSubtitle(extras.getString(MediaMetadataCompat.METADATA_KEY_ARTIST))
+                            .setArtworkUri(Uri.parse(extras.getString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI)))
+                            .build()
+                    )
+                    .build()
+            }
+        }
+    }
+
+    private inner class PlayerNotificationListener :
+        PlayerNotificationManager.NotificationListener {
+        override fun onNotificationPosted(
+            notificationId: Int,
+            notification: Notification,
+            ongoing: Boolean
+        ) {
+            if (ongoing && !isForeground) {
+                ContextCompat.startForegroundService(
+                    applicationContext,
+                    Intent(applicationContext, this@GoPlayerService.javaClass)
+                )
+                startForeground(notificationId, notification)
+                isForeground = true
+            }
+        }
+
+        override fun onNotificationCancelled(notificationId: Int, dismissedByUser: Boolean) {
+            stopForeground(true)
+            isForeground = false
+            stopSelf()
         }
     }
 
---
+++ app/src/main/java/com/colisa/podplay/player/NotificationManager.kt
@@ -0,0 +1,111 @@
+package com.colisa.podplay.player
+
+import android.app.PendingIntent
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.colisa.podplay.R
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import kotlinx.coroutines.*
+
+private const val NOW_PAYING_CHANNEL_ID = "com.colisa.gopods.NOW_PLAYING"
+private const val NOW_PAYING_NOTIFICATION_ID = 0xb339
+
+class NotificationManager(
+    private val context: Context,
+    private val sessionToken: MediaSessionCompat.Token,
+    private val notificationListener: PlayerNotificationManager.NotificationListener
+) {
+    private var player: Player? = null
+    private val job = SupervisorJob()
+    private val notifyScope = CoroutineScope(Dispatchers.Main + job)
+    private val notificationManager: PlayerNotificationManager
+
+    init {
+        val mediaController = MediaControllerCompat(context, sessionToken)
+        notificationManager = PlayerNotificationManager.Builder(
+            context,
+            NOW_PAYING_NOTIFICATION_ID,
+            NOW_PAYING_CHANNEL_ID
+        )
+            .setChannelNameResourceId(R.string.np_notification_channel)
+            .setChannelDescriptionResourceId(R.string.np_notification_channel_description)
+            .setMediaDescriptionAdapter(DescriptionAdapter(mediaController))
+            .setNotificationListener(notificationListener)
+            .build().apply {
+                setMediaSessionToken(sessionToken)
+                setSmallIcon(R.drawable.ic_episode_play_circle)
+                setUseRewindAction(true)
+                setUseFastForwardAction(true)
+            }
+    }
+
+    fun hideNotification() {
+        notificationManager.setPlayer(null)
+    }
+
+    fun showNotificationForPlayer(player: Player) {
+        notificationManager.setPlayer(player)
+    }
+
+    private inner class DescriptionAdapter(private var controller: MediaControllerCompat) :
+        PlayerNotificationManager.MediaDescriptionAdapter {
+
+        var currentIconUri: Uri? = null
+        var currentBitmap: Bitmap? = null
+
+
+        override fun getCurrentContentTitle(player: Player): CharSequence =
+            player.mediaMetadata.title.toString()
+
+        override fun createCurrentContentIntent(player: Player): PendingIntent? =
+            controller.sessionActivity
+
+
+        override fun getCurrentContentText(player: Player): CharSequence =
+            player.mediaMetadata.subtitle.toString()
+
+
+        override fun getCurrentLargeIcon(
+            player: Player,
+            callback: PlayerNotificationManager.BitmapCallback
+        ): Bitmap? {
+            val iconUri = player.mediaMetadata.artworkUri
+            return if (currentIconUri != iconUri || currentBitmap == null) {
+                // Cache the bitmap for current song
+                currentIconUri = iconUri
+                notifyScope.launch {
+                    currentBitmap = iconUri?.let {
+                        resolveUriAsBitmap(it)
+                    }
+                    currentBitmap?.let { callback.onBitmap(it) }
+                }
+                null
+            } else {
+                currentBitmap
+            }
+        }
+
+        private suspend fun resolveUriAsBitmap(uri: Uri): Bitmap? {
+            return withContext(Dispatchers.IO) {
+                Glide.with(context).applyDefaultRequestOptions(glideOptions)
+                    .asBitmap()
+                    .load(uri)
+                    .submit(NOTIFICATION_LARGE_ICON_SIZE, NOTIFICATION_LARGE_ICON_SIZE)
+                    .get()
+            }
+        }
+
+    }
+}
+
+const val NOTIFICATION_LARGE_ICON_SIZE = 80
+private val glideOptions = RequestOptions()
+    .fallback(R.drawable.album_art)
+    .diskCacheStrategy(DiskCacheStrategy.DATA)
