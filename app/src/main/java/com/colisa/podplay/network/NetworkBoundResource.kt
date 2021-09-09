dffe8a7029923aeba4bfd74824dc3fdf766b5427
mtali <emmanuelmtali@yahoo.com>
2021-09-10 01:22:29 +0300

    Add network bound resource for caching network result to database

---
+++ app/src/main/java/com/colisa/podplay/network/NetworkBoundResource.kt
@@ -0,0 +1,27 @@
+package com.colisa.podplay.network
+
+import kotlinx.coroutines.flow.*
+
+inline fun <ResultType, RequestType> networkBoundResource(
+    crossinline query: () -> Flow<ResultType>,
+    crossinline fetch: suspend () -> RequestType,
+    crossinline saveFetchResult: suspend (RequestType) -> Unit,
+    crossinline onFetchFailed: (Throwable?) -> Unit = { },
+    crossinline shouldFetch: (ResultType) -> Boolean = { true }
+) = flow {
+    emit(Resource.Loading(null))
+    val data = query().first()
+    val flow = if (shouldFetch(data)) {
+        emit(Resource.Loading(data))
+        try {
+            saveFetchResult(fetch())
+            query().map { Resource.Success(it) }
+        } catch (throwable: Throwable) {
+            onFetchFailed(throwable)
+            query().map { Resource.Error(throwable, it) }
+        }
+    } else {
+        query().map { Resource.Success(it) }
+    }
+    emitAll(flow)
+}
